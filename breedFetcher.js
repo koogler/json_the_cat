a373477fbf50843f5b94d4b5cd39546f28f028ba
Desmond Parker <evan.parker95@gmail.com>
2021-12-02 23:44:09 +0000

    adding breedFetcher refactoring, and breedFetcherTest functionality with mocha chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,39 @@
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (err, response, body) => {
-  const data = JSON.parse(body);
-  if (err) {
-    console.error(err);
-    return;
-  }
-  if (data[0] === undefined) {
-    console.log("BREED NOT FOUND!!!");
-  } else {
-    console.log(data[0]['description']);
-    console.log(typeof data);
-  }
-});
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (err, response, body) => {
+      let data = JSON.parse(body);
+      if (data[0] === undefined) {
+        return callback(err, null);
+      }
+      if (!err) {
+        let result = data[0]['description'];
+        return callback(null, result);
+      }
+    });
+};
+
+module.exports = { fetchBreedDescription };
+
+// request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (err, response, body) => {
+//   const data = JSON.parse(body);
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   if (data[0] === undefined) {
+//     console.log("BREED NOT FOUND!!!");
+//   } else {
+//     console.log(data[0]['description']);
+//     console.log(typeof data);
+//   }
+// });
+
+// if (err) {
+//   console.error(err);
+//   return;
+// }
+// const desc = JSON.parse(body);
+// return desc;
+// })
